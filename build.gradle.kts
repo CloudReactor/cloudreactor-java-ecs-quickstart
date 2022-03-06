b6760cd40d3af64f28ecb961d6f1792efcca74d5
Jeff Tsay <jctsay@aim.com>
2022-03-05 23:42:42 -0800

    Upgrade CloudReactor tasksymphony to 0.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
   implementation("org.apache.logging.log4j", "log4j-api", log4j2Version)
   implementation("org.apache.logging.log4j", "log4j-core", log4j2Version)
   implementation("org.apache.logging.log4j", "log4j-slf4j-impl", log4j2Version)
-  implementation("io.cloudreactor", "tasksymphony", "0.4.0")
+  implementation("io.cloudreactor", "tasksymphony", "0.5.0")
   testImplementation(platform("org.junit:junit-bom:5.8.2"))
   testImplementation("org.junit.jupiter:junit-jupiter")
 }
